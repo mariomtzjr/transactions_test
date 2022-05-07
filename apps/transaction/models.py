e8643545ea5bc7555172d1718f0d9f64f55d7406
Mario Martinez <francisco.smtz@gmail.com>
2022-05-07 00:30:02 -0500

    Add method to set final_payment field value into Transaction model.

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -18,16 +18,23 @@ class Transaction(models.Model):
     company_id = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(default=timezone.now)
-    status = models.CharField(
+    status_transaction = models.CharField(
         max_length=20,
         choices=TRANSACTION_STATUS_CHOICES,
         default='pending',)
-    aprove = models.BooleanField(default=False)
+    status_approved = models.BooleanField(default=False)
+    final_payment = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.company_id.name
+    
+    def get_final_payment(self):
+        if self.status_transaction == 'closed' and self.status_approved == True:
+            self.final_payment = True
+        return self.final_payment
+
 
 
 class TransactionStatus(models.Model):

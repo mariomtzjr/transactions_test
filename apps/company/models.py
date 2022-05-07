6e43d6e66b0021b6938108a7c62460a51dbbed91
Mario Martinez <francisco.smtz@gmail.com>
2022-05-07 00:07:52 -0500

    Add base fields into Transaction,Company models.

--- apps/company/models.py
+++ apps/company/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
+
+COMPANY_STATUS_CHOICES = [
+    ('active', 'Active'),
+    ('inactive', 'Inactive'),
+]
+
+
 # Create your models here.
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    company_estatus = models.CharField(
+        max_length=20,
+        choices=COMPANY_STATUS_CHOICES,
+        default='active',)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -1,3 +1,35 @@
+import uuid
+from datetime import datetime
+
 from django.db import models
 
+
+TRANSACTION_STATUS_CHOICES = [
+    ('closed', 'Closed'),
+    ('reversed', 'Reversed'),
+    ('pending', 'Pending'),
+]
+
+
 # Create your models here.
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_id = models.ForeignKey('apps.Company', on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(default=datetime.now())
+    status = models.CharField(
+        max_length=20,
+        choices=TRANSACTION_STATUS_CHOICES,
+        default='pending',)
+    aprove = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.company_id.name
+
+
+class TransactionStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)

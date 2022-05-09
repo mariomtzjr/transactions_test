41e5451b121a4e4538169ffa1f529007eb9be10b
Mario Martinez <francisco.smtz@gmail.com>
2022-05-08 22:54:17 -0500

    Add redirect view for root

--- transactions/urls.py
+++ transactions/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.index.views import redirect_view
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('', redirect_view, name='redirect-view'),
 ]
